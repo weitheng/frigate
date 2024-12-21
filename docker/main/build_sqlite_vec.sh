71fdd30128771a37de8288aaea5a511565b609cc
Weitheng Haw <wei.thenghaw@disneystreaming.com>
2024-12-21 14:38:25 +0000

    Revert "Update base image (#15103)"
    This reverts commit 73332cb8

--- docker/main/build_sqlite_vec.sh
+++ docker/main/build_sqlite_vec.sh
@@ -4,7 +4,8 @@ set -euxo pipefail
 
 SQLITE_VEC_VERSION="0.1.3"
 
-sed -i '/^Types:/s/deb/& deb-src/' /etc/apt/sources.list.d/debian.sources
+cp /etc/apt/sources.list /etc/apt/sources.list.d/sources-src.list
+sed -i 's|deb http|deb-src http|g' /etc/apt/sources.list.d/sources-src.list
 apt-get update
 apt-get -yqq build-dep sqlite3 gettext git
 
