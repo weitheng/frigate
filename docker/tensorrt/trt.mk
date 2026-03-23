2f168c523940d365d5e5806f33d2999e7eecec07
Wei Theng <34220268+weitheng@users.noreply.github.com>
2026-03-23 15:41:52 +0800

    Add legacy GPU build variant for older Nvidia GPUs (#30)
    Parameterize the TensorRT amd64 requirements file so users with
    older Nvidia GPUs (Pascal, Volta, Turing) can build an image using
    CUDA 12.5 / cuDNN 9.3 / onnxruntime-gpu 1.22, which still supports
    compute capability < 8.0.
    
    Build with: make local-trt-legacy
    
    Co-authored-by: Weitheng Haw <wei.thenghaw@disneystreaming.com>

--- docker/tensorrt/trt.mk
+++ docker/tensorrt/trt.mk
@@ -3,12 +3,18 @@ BOARDS += trt
 JETPACK5_BASE ?= nvcr.io/nvidia/l4t-tensorrt:r8.5.2-runtime	# L4T 35.3.1 JetPack 5.1.1
 JETPACK6_BASE ?= nvcr.io/nvidia/tensorrt:23.12-py3-igpu
 X86_DGPU_ARGS := ARCH=amd64 COMPUTE_LEVEL="50 60 70 80 90"
+X86_DGPU_LEGACY_ARGS := ARCH=amd64 COMPUTE_LEVEL="50 60 70 80 90" TRT_REQUIREMENTS=requirements-amd64-legacy.txt
 JETPACK5_ARGS := ARCH=arm64 BASE_IMAGE=$(JETPACK5_BASE) SLIM_BASE=$(JETPACK5_BASE) TRT_BASE=$(JETPACK5_BASE)
 JETPACK6_ARGS := ARCH=arm64 BASE_IMAGE=$(JETPACK6_BASE) SLIM_BASE=$(JETPACK6_BASE) TRT_BASE=$(JETPACK6_BASE)
 
 local-trt: version
 	$(X86_DGPU_ARGS) docker buildx bake --file=docker/tensorrt/trt.hcl tensorrt \
 		--set tensorrt.tags=frigate:latest-tensorrt \
+		--load
+
+local-trt-legacy: version
+	$(X86_DGPU_LEGACY_ARGS) docker buildx bake --file=docker/tensorrt/trt.hcl tensorrt \
+		--set tensorrt.tags=frigate:latest-tensorrt-legacy \
 		--load
 
 local-trt-jp5: version
